5d577fa9bb9abc6eed4a67e1918424b65a290245
Leandro Acquati <leandroacquati@gmail.com>
2018-06-28 21:26:46 -0300

    arrumando o responsivo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,17 @@
 <div class="header">
-	<h1>Portfólio Grupo Tesseract</h1>
+	<h1>Portfólio<br class="viuva"> Grupo Tesseract</h1>
 
-	<a href="https://github.com/grupotesseract" target="_blank">
-		<i class="fab fa-github-square fa-2x"></i><p>/grupotesseract</p>
-	</a>
+	<div class="links-header">
+		<a href="https://www.grupotesseract.com.br/" target="_blank">
+			<i class="fas fa-home fa-2x"></i><p>grupotesseract.com.br</p>
+		</a>
 
-	<a href="https://github.com/grupotesseract/gerador-de-postagem" target="_blank">
-		<i class="fab fa-github-square fa-2x"></i><p>/gerador-de-postagem</p>
-	</a>	
+		<a href="https://github.com/grupotesseract" target="_blank">
+			<i class="fab fa-github-square fa-2x"></i><p>grupotesseract</p>
+		</a>
+
+		<a href="https://github.com/grupotesseract/gerador-de-postagem" target="_blank">
+			<i class="fab fa-github-square fa-2x"></i><p>gerador-de-postagem</p>
+		</a>
+	</div>	
 </div>
